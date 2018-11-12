f1ce9f88e9f4a1c83a05e61fca32ed0910585994
jvpereirad <42525235+jvpereirad@users.noreply.github.com>
2018-11-12 19:47:22 -0200

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,32 @@
+/* P4.c */
+/* Joao Victor Pereira Delfino */
+/* 11811EAU018 */
+
+#include <stdio.h>
+
+long long int ackermann ( int a, int b)
+{
+	if( a > 0 && b > 0 )
+	{
+		return ackermann(a-1, ackermann(a , b-1));
+	}
+	else if( a > 0 && b == 0)
+	{
+		return ackermann( a - 1 , 1 );
+	}
+	else if(a == 0)
+	{
+		return b+1;
+	}
+}
+
+int main()
+{
+	int m, n, res = 0;
+	printf("Digite um valor para m: ");
+	scanf("%i", &m);
+	printf("\nDigite um valor para n: ");
+	scanf("%i", &n);
+	printf("\nA(%d,%d) = %lld", m , n, ackermann(m,n) );
+	return 0;
+}
